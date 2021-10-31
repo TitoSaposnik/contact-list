7224c89198bc1f52ddf48f07a06a1afa2c43fa47
Tito Saposnik <tito.saposnik@gmail.com>
2021-10-31 15:43:10 -0300

    Agregando mas funciones

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,18 @@ const nombre = document.querySelector(".nombre")
 const numero = document.querySelector(".numero")
 const direccion = document.querySelector(".direccion")
 const btnAgregarTarea = document.querySelector(".btn-agregar-tarea")
-const listadoTareas = document.querySelectorAll(".listado-tarea")
+
+const listadoTareas = document.querySelectorAll(".listado-tareas")
+
 const db = window.localStorage
 
 btnAgregarTarea.onclick = () =>{
     let contacto = {
-        id:Math.random(1,100) ,
+        id:Math.random(1,100),
         nombre: nombre.value,
         numero: numero.value,
-        direccion: direccion.value
+        direccion: direccion.value,
     }
-    console.log(contacto)
+    guardarContacto(db,contacto)
 }
+cargarContactos(db, listadoTareas)
